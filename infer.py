ad659b28bd91bbeaad88e865bff0f7479d3d90f4
Dor Zohar <dorzohar@gmail.com>
2017-01-15 22:07:05 +0200

    Keep vector scores of selected iteerations

--- infer.py
+++ infer.py
@@ -2,8 +2,8 @@ import numpy as np
 import time
 from chu_liu import mst
 from features import parse_input_file
-from learn import sentence_to_graph, get_weighted_graph, mst_wrapper
-from vector import vec
+from learn import sentence_to_graph, get_weighted_graph, perceptron, vector_location
+#from vector import vec
 
 
 def get_inference_accuracy(sentence_and_tree):
@@ -16,21 +16,37 @@ def get_inference_accuracy(sentence_and_tree):
     return sum
 
 
-def infer(file_path, families):
+def infer(file_path, families, vector):
     start_time = time.time()
     sentences = parse_input_file(file_path)
     word_num = sum([len(sentence) for sentence in sentences])
     graphs = map(lambda sentence: sentence_to_graph(sentence, families), sentences)
-    selected_vec = np.asarray(vec[len(vec) - 1])
+    selected_vec = np.asarray(vector)
     weighted_graphs = list(map(lambda graph: get_weighted_graph(graph, selected_vec), graphs))
-    msts = map(lambda wgraph: mst_wrapper(0, wgraph), weighted_graphs)
+    msts = map(lambda wgraph: mst(0, wgraph), weighted_graphs)
     accuracies = map(get_inference_accuracy, zip(sentences, msts))
 
     total_acc = sum(list(accuracies)) / float(word_num)
     print("accuracy: " + str(total_acc*100) + "%")
     print("time: " + str(time.time()-start_time) + " seconds")
+    return total_acc, time.time()-start_time
+
+
+
+def train_and_infer(file_name, families, iterations = [20, 50, 80, 100]):
+    vecs_by_iter = perceptron(file_name, iterations, families)
+    results = {}
+    for i, vec in vecs_by_iter.items():
+        results[i] = infer('test.labeled', families, vec)
+
+    output_path = vector_location(families) + "/results.csv"
+    with open(output_path, 'w') as file:
+        file.write("Iteration,Accuracy,Infer_Time\n")
+        for i, res in results.items():
+            file.write("%d,%f,%f\n" % (i, 100*res[0], res[1]))
 
 
 
 if __name__ == "__main__":
-    infer("test.labeled", [1, 2, 3, 4, 5, 6, 8, 10, 13, 15, 16, 17, 18, 19, 20])
+#    infer("test.labeled", [1, 2, 3, 4, 5, 6, 8, 10, 13, 15, 16, 17, 18, 19, 20])
+    train_and_infer("train.labeled", [1, 2, 3, 4, 5, 6, 8, 10, 13])
--- learn.py
+++ learn.py
@@ -3,6 +3,7 @@ import time
 from chu_liu import mst
 from features import parse_input_file, root_word, get_edge_features, total_feature_num
 import numpy as np
+import os
 
 
 def sentence_to_graph(sentence, families):
@@ -58,16 +59,21 @@ def compare_labels_to_graph(sentence, graph):
     return True
 
 
-def mst_wrapper(root, graph):
-    true_root = min(graph[0], key = graph[0].get)
-    root_value = graph[0][true_root]
-    del graph[0]
-    mst_tree = mst(true_root, graph)
-    mst_tree[0] = {true_root: root_value}
-    return mst_tree
+def vector_location(families):
+    return "vectors/%s" % '_'.join([str(family) for family in families])
 
 
-def perceptron(file_path, n, families):
+def keep_vector(vec, families, iter):
+    dir = vector_location(families)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    file_name = dir + "/vector_%d.py" % iter
+    with open(file_name, "w") as f:
+        f.write("vec = %s\n" % vec.tolist())
+
+
+def perceptron(file_path, iterations, families):
+    n = max(iterations) + 1
     start_time = time.time()
     sentences = parse_input_file(file_path)
     graphs = list(map(lambda sentence: (sentence, sentence_to_features(sentence, families), sentence_to_graph(sentence, families)), \
@@ -75,25 +81,25 @@ def perceptron(file_path, n, families):
     vec = np.zeros(total_feature_num(families))
     print("%d sentences\n" % len(sentences))
 
-    res_file = open("vector.py", 'w')
-    res_file.write("vec = {}\n\n")
+    vecs_by_iter = {}
 
     for i in range(0, n):
         print("enter %d iteration at %f" % (i, time.time() - start_time))
         for sentence, features, graph in graphs:
             weighted_graph = get_weighted_graph(graph, vec)
-            graph_mst = mst_wrapper(0, weighted_graph)
+            graph_mst = mst(0, weighted_graph)
             if not compare_labels_to_graph(sentence, graph_mst):
                 graph_features = graph_to_features(graph_mst, sentence, families)
                 for feature in features:
                     vec[feature] += 1
                 for feature in graph_features:
                     vec[feature] -= 1
-        res_file.write("vec[%d] = %s\n" % (i, vec.tolist()))
+        if i in iterations:
+            vecs_by_iter[i] = vec.tolist()
+            keep_vector(vec, families, i)
 
+    return vecs_by_iter
 
-    res_file.close()
-    return vec
 
 if __name__ == "__main__":
-    perceptron("train.labeled", 5, [1, 2, 3, 4, 5, 6, 8, 10, 13, 15, 16, 17, 18, 19, 20])
+    perceptron("train.labeled", [5], [1, 2, 3, 4, 5, 6, 8, 10, 13, 15, 16, 17, 18, 19, 20])
